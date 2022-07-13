d96466ad744eef7cc89a8ed6bad014251741a5f2
EC2 Default User <ec2-user@ip-172-31-20-227.ec2.internal>
2022-07-13 10:03:44 +0000

    Reservation function developed capacity limit / error messages

--- app/controllers/public/reservations_controller.rb
+++ app/controllers/public/reservations_controller.rb
@@ -6,18 +6,49 @@ class Public::ReservationsController < ApplicationController
     @day = params[:day]
     @time = params[:time_from]
     @start_time = DateTime.parse(@day + " " + "00:00" + " " + "JST")
+    @reservations = Reservation.where(day: @day, time_from: @time)
     message = Reservation.check_reservation_day(@day.to_date)
     if !!message
       redirect_to @reservation, flash: { alert: message }
     end
   end
-  
+
   def index
     @user = current_user
-  end  
+    @reservations = Reservation.where(user_id: @user.id).order(day: :desc)
+  end
 
   def show
     @reservation = Reservation.find(params[:id])
+  end
+
+  def edit
+    @reservation = Reservation.find(params[:id])
+    @reservations = Reservation.where(day: @reservation.day, time_from: @reservation.time_from)
+  end
+
+  def update
+    @reservation = Reservation.find(params[:id])
+    reservations = Reservation.reservations_after_three_month
+    @reservations = Reservation.where(day: @day, time_from: @time, user_id: @reservation.user_id)
+    number_of_ppl = 0
+      @reservations.each do |r|
+        number_of_ppl += r.number_of_ppl
+      end
+    @total = view_context.check_reservation(reservations, @reservation.day, @reservation.time_from) - number_of_ppl + @reservation.number_of_ppl
+    if @total <= 10
+        @reservation.update(reservation_params)
+        redirect_to reservation_path(@reservation.id)
+    else
+        flash[:notice] = "定員オーバーです。"
+        redirect_to root_path
+    end
+  end
+
+  def destroy
+    @reservation = Reservation.find(params[:id])
+    @reservation .destroy
+    redirect_to reservations_path
   end
 
   def create
@@ -26,16 +57,15 @@ class Public::ReservationsController < ApplicationController
     @reservation.user_id = current_user.id
     reservations = Reservation.reservations_after_three_month
     if Reservation.find_by(day: @reservation.day, time_from: @reservation.time_from, user_id: @reservation.user_id).present?
-        render :index
-    elsif view_context.check_reservation(reservations, @reservation.day, @reservation.time_from) + @reservation.number_of_ppl < 13
-      if @reservation.save
-        redirect_to reservation_path @reservation.id
-      else
-        render :new
-      end
+      flash[:notice] = "既に予約が存在します。変更の場合はキャンセル後に再度予約手続きをお願いいたします"
+      redirect_to reservations_path
+    elsif view_context.check_reservation(reservations, @reservation.day, @reservation.time_from) + @reservation.number_of_ppl <= 10
+      @reservation.save
+      redirect_to reservation_path(@reservation.id)
     else
-        render :new
-    end  
+      flash[:notice] = "エラーが発生しました。"
+      redirect_to root_path
+    end
   end
 
 private
